848762c04dd947b1fb413bc6c3a3f9fe0e873094
Anders Clausen <anders.clausen1@maersk.com>
2021-03-26 16:38:46 +0000

    Initial commit

---
+++ src/main/java/com/example/sleuthissue/SleuthIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.sleuthissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sleuthissue/SleuthIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sleuthissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
